508e85e3a3c1900b5703a6418aa9179336268ba1
AliHazim12 <bluegamer1098@gmail.com>
2026-05-11 05:21:31 -0400

    Initial Spring Boot setup

---
+++ src/main/java/com/alihazim/careertrackr/CareertrackrApplication.java
@@ -0,0 +1,13 @@
+package com.alihazim.careertrackr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareertrackrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CareertrackrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alihazim/careertrackr/CareertrackrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alihazim.careertrackr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CareertrackrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
